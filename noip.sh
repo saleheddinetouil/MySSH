1f8dbe13caeb8737f7dfb4078bce3ff4f2baf0bf
Saleh eddine Touil <168127706+saleheddinetouil@users.noreply.github.com>
2024-11-17 11:35:00 +0100

    Update noip.sh

--- noip.sh
+++ noip.sh
@@ -7,3 +7,6 @@ HOSTNAME="$NOIP_HOSTNAME"
 
 # Update No-IP
 curl -u $USERNAME:$PASSWORD "http://dynupdate.no-ip.com/nic/update?hostname=$HOSTNAME"
+
+# Display the No-IP hostname
+echo "No-IP hostname updated: $HOSTNAME"
